26db0cd353199e909f986dc0a48399a15aca2e78
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-24 14:53:16 +0000

    rising_beyondx: adapt to rising

--- rising_beyondx.mk
+++ rising_beyondx.mk
@@ -34,13 +34,13 @@ TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_beyondx
+PRODUCT_NAME := rising_beyondx
 PRODUCT_DEVICE := beyondx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G977B
